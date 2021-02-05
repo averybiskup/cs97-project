4fc37236cc8fc6bf7578dad10a1eed5b29cef9d0
Avery Biskup <averybiskup@gmail.com>
2021-02-05 13:18:54 -0800

    added documentation

--- src/fetchCourses.js
+++ src/fetchCourses.js
@@ -1,7 +1,13 @@
+// Fetching data from the server
+
 import axios from 'axios'
 
 const fetchCourses = async () => {
     const key = process.env.SERVER_KEY
+
+
+    // We can use /api/test because we have a proxy in package.json which
+    // points to the server address (localhost:8000)
     const data = await axios.get('/api/test')
         
         .then(res => {
