0f9dd93922307a68af44fdca5141d9cebba70c23
Avery Biskup <averybiskup@gmail.com>
2021-02-23 12:41:20 -0800

    removed mime

--- src/CreateReview.js
+++ src/CreateReview.js
@@ -10,7 +10,7 @@ import ReviewCard from './ReviewCard.js'
 
 // Function for handling the submit button on the form
 // Returns true if form was valid
-const handleSubmit = (e, course_id, author, title, body, rating, setMessage) => {
+const handleSubmit = (e, course_id, title, body, rating, setMessage) => {
     e.preventDefault()
 
     // Checking for valid form data
@@ -21,26 +21,31 @@ const handleSubmit = (e, course_id, author, title, body, rating, setMessage) => 
     } else if (rating == 0) {
         alert('Rating can not be 0')
     } else {
-        if (author.length == 0) {
-            author = 'Anon'
+
+        const author = window.localStorage.getItem('username')
+        
+        if (!author) {
+            alert('You are not signed in')
+            window.location.replace('/cs97-project/login')
+        } else {
+
+            // Posting review
+            postReview(course_id, body, author, title, rating)
+
+            // Rerendering parent component
+            setMessage('Preparing review...')
+
+            // Fetching courses and updating localStorage
+            //fetchCourses()
+            fetchReviews(course_id)
+
+            // Timeout so we have data before rerendering parent component
+            setTimeout(() => {
+
+                setMessage('Review added!')
+            }, 5000)
+            return true
         }
-
-        // Posting review
-        postReview(course_id, body, author, title, rating)
-
-        // Rerendering parent component
-        setMessage('Preparing review...')
-
-        // Fetching courses and updating localStorage
-        //fetchCourses()
-        fetchReviews(course_id)
-
-        // Timeout so we have data before rerendering parent component
-        setTimeout(() => {
-
-            setMessage('Review added!')
-        }, 5000)
-        return true
     }
     return false
 }
@@ -50,7 +55,6 @@ const handleSubmit = (e, course_id, author, title, body, rating, setMessage) => 
 const CreateReview = (props) => {
 
     // State variables that are changed when form gets input
-    const [author, setAuthor] = useState('Anon')
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [rating, setRating] = useState(0)
@@ -59,16 +63,14 @@ const CreateReview = (props) => {
     return (
         <div className='create-review'>
             <form id='createreview' onSubmit={(e) => { 
-                if (handleSubmit(e, course_id, author, title, body, rating, props.updateMessage)) {
+                if (handleSubmit(e, course_id, title, body, rating, props.updateMessage)) {
                     // Resetting form inputs
-                    setAuthor('Anon')
                     setTitle('')
                     setBody('')
                     setRating(0)
                 }
             }}>
                 
-                <input type='text' name='author' placeholder='name' value={author} onChange={(e) => setAuthor(e.target.value)}/>
                 <input type='text' name='title' placeholder='title (required)' value={title} onChange={(e) => setTitle(e.target.value)} />
                 <input type='text' name='body' placeholder='review (required)' value={body} onChange={(e) => setBody(e.target.value)} />
                 <select value={rating} onChange={(e) => setRating(e.target.value)}>
