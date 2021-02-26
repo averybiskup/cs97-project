a3d688ad539148e74c7329a0e2acf36b660eabed
MUSK SWEET <rokij6698@gmail.com>
2021-02-26 12:42:10 -0800

    added RanderReviews.js

---
+++ src/RenderReviews.js
@@ -0,0 +1,36 @@
+import ReviewCard from './ReviewCard.js'
+
+let RenderReviews = (props) => {
+
+
+    // Getting courses object from localStorage
+    const courses = JSON.parse(window.localStorage.getItem('courses'))
+    const reviews = courses[props.loc]['reviews']
+
+    // Making sure that a course has at least 1 review, otherwise
+    // displays this message
+    if (!reviews) {
+        return (
+            <div>
+                Course has no reviews.
+            </div>
+        )
+    }
+
+    // Turning object with reviews, into list of reviews
+    const reviewsList = Object.keys(reviews).map((id) => {
+       return reviews[id]
+    })
+    
+    return (
+        <div className="reviews">
+            <div>{props.message}</div>
+            <h1>Reviews:</h1>
+            {reviewsList.map((review) => {
+                return <ReviewCard key={review.id} review={review} />
+            })}
+        </div>
+    )
+}
+
+export default RenderReviews
